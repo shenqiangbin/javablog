1cfd478c7e92302576ed0e3937d1b4928c4075f8
shenqiangbin <1348907384@qq.com>
2018-06-25 22:46:47 +0800

    权限验证

---
+++ src/main/java/com/sqber/blog/security/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.sqber.blog.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
