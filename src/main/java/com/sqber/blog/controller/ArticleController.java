417c254a8caca20ec8de87f5588210e2b056ddd7
shenqiangbin <shenqiangbin@163.com>
2018-11-26 17:28:31 +0800

    eidtor.md 赋值 和 取值

--- src/main/java/com/sqber/blog/controller/ArticleController.java
+++ src/main/java/com/sqber/blog/controller/ArticleController.java
@@ -1,13 +1,23 @@
 package com.sqber.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ArticleController {
 	
 	@GetMapping("/article/add")
-	public String add() {	
+	public String add(Model model) {	
+		
+		model.addAttribute("content","##title");
 		return "article/add";
+	}
+	
+	@GetMapping("/article/view")
+	public String view(Model model) {	
+		
+		model.addAttribute("content","##title");
+		return "article/view";
 	}
 }
